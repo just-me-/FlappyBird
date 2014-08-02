79d3753b5b17c04b13d66dcd59f17bb497f09392
just-me- <hess-marcel@bluewin.ch>
2014-08-02 10:44:04 +0200

    Projekterstellung Xcode

---
+++ Flappy Bird/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flappy Bird
+//
+//  Created by Marcel Hess on 02.08.14.
+//  Copyright (c) 2014 Marcel Hess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Flappy Bird/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Flappy Bird
+//
+//  Created by Marcel Hess on 02.08.14.
+//  Copyright (c) 2014 Marcel Hess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
