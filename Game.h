aaeb9abb20f636bc1b82fe398fee255846221634
just-me- <hess-marcel@bluewin.ch>
2014-08-02 11:21:02 +0200

    Grundgerüst Interface

---
+++ Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  Flappy Bird
+//
+//  Created by Marcel Hess on 02.08.14.
+//  Copyright (c) 2014 Marcel Hess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Game : UIViewController
+
+@end
